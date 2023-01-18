6b5eaefa936c01e550fa377a98a9d3b8fc47b28b
KatarzynaGrzeskowiak <64362085+KatarzynaGrzeskowiak@users.noreply.github.com>
2023-01-18 15:03:14 +0100

    Update isPrime

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -202,8 +202,15 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (num == 2 || num == 3)
+    return true;
+  if (num <= 1 || num % 2 == 0 || num % 3 == 0)
+    return false;  
+  for (let i = 5; i * i <= num ; i+=6)
+    if (num % i == 0 || num % (i + 2) == 0)
+      return false;
+  return true;
 }
 
 /**
