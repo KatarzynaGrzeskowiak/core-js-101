488f34f99f1b098ad760c6960853fc281e0081e7
KatarzynaGrzeskowiak <64362085+KatarzynaGrzeskowiak@users.noreply.github.com>
2023-01-20 15:48:09 +0100

    Update generateOdds

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -36,7 +36,7 @@ function findElement(arr, value) {
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
 function generateOdds(len) {
- return Array(len).fill().map((element, index) => (index + 1) * 2 - 1);
+  return Array(len).fill().map((element, index) => (index + 1) * 2 - 1);
 }
 
 
