f930c10a1a0fdf877172bb5832fec69efb658bca
KatarzynaGrzeskowiak <64362085+KatarzynaGrzeskowiak@users.noreply.github.com>
2023-01-23 23:00:10 +0100

    Update sortDigitNamesByNumericOrder

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -326,8 +326,9 @@ function getPositivesCount(arr) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const numberAsWords = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  return arr.sort((a, b) => numberAsWords.indexOf(a) - numberAsWords.indexOf(b));
 }
 
 /**
