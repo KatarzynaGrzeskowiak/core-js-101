f6405f5ddd0f00d1a4150ac7d57356cb79a38523
KatarzynaGrzeskowiak <64362085+KatarzynaGrzeskowiak@users.noreply.github.com>
2023-01-23 23:36:36 +0100

    Update reverseString, reverseInteger

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -213,8 +213,8 @@ function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
  * 'rotator' => 'rotator'
  * 'noon' => 'noon'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return str.split('').reverse().join('');
 }
 
 
@@ -230,8 +230,8 @@ function reverseString(/* str */) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return num.toString(10).split('').reverse().join('');
 }
 
 
