4ed1baf020b4fe99e47d3728347c1f28036d744b
KatarzynaGrzeskowiak <64362085+KatarzynaGrzeskowiak@users.noreply.github.com>
2023-01-23 23:21:57 +0100

    Update swapHeadAndTail

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -584,8 +584,13 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const head = arr.slice(0, Math.floor(arr.length / 2));
+  let tail = arr.slice(-Math.ceil(arr.length / 2));
+  if (arr.length % 2 === 1) {
+    tail = tail.slice(1).concat(tail[0]);
+  }
+  return tail.concat(head);
 }
 
 
